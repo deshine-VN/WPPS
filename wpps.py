860fc79e9199a7be38291331897adc80b18fc500
Deshine <duc.hacker.ngo@gmail.com>
2024-03-20 22:52:08 +0700

    Update wpps.py

--- wpps.py
+++ wpps.py
@@ -5,20 +5,6 @@ from update import Update
 from check import Check
 
 def main():
-    print(r"""
-__        __            _ ____                                           
-\ \      / /__  _ __ __| |  _ \ _ __ ___  ___ ___                        
- \ \ /\ / / _ \| '__/ _` | |_) | '__/ _ \/ __/ __|                       
-  \ V  V / (_) | | | (_| |  __/| | |  __/\__ \__ \                       
- __\_/\_/ \___/|_|  \__,_|_|   |_| _\___||___/___/                       
-|  _ \| |_   _  __ _(_)_ __  ___  / ___|  ___ __ _ _ __  _ __   ___ _ __ 
-| |_) | | | | |/ _` | | '_ \/ __| \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
-|  __/| | |_| | (_| | | | | \__ \  ___) | (_| (_| | | | | | | |  __/ |   
-|_|   |_|\__,_|\__, |_|_| |_|___/ |____/ \___\__,_|_| |_|_| |_|\___|_|   
-               |___/                                                     
-                        Author: deshine (https://hackerone.com/deshine)
-""")
-    
     parser = argparse.ArgumentParser(description="Usage:")
     sub_parsers = parser.add_subparsers(dest="module")
     
